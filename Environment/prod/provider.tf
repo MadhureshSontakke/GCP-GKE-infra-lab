e93115c03a6eeb3c71e1c5a1c139b4d42f3047a0
Madhuresh <madhuresh.sontakke@gmail.com>
2026-06-29 19:25:02 +0530

    changed workflow and removed provideer creds

--- Environment/prod/provider.tf
+++ Environment/prod/provider.tf
@@ -12,5 +12,5 @@ provider "google" {
   region  = var.my_region
   # zone    = var.my_zone
   # use a string path; path.module ensures the file is looked up next to these .tf files
-  credentials = file("${path.module}/creds.json")
+  # credentials = file("${path.module}/creds.json")
 }
