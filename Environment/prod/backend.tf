28d5ca3a966c87c3240d19f57f2d2379a4a33b73
Madhuresh <madhuresh.sontakke@gmail.com>
2026-06-29 19:05:48 +0530

    backend creds removed

--- Environment/prod/backend.tf
+++ Environment/prod/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "terraform-backend-2099-5"
     prefix = "terraform/prod/state"
     # Point the backend to the same service account credentials file (literal path required)
-    credentials = "creds.json"
+    # credentials = "creds.json"
   }
 }
