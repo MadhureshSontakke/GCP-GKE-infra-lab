5c885a8f5efa953e6f1b2e6c776fbb31808bbbc9
Madhuresh <madhuresh.sontakke@gmail.com>
2026-06-30 17:58:48 +0530

    corrected the new changes

--- modules/GKE/main.tf
+++ modules/GKE/main.tf
@@ -13,7 +13,7 @@ resource "google_container_cluster" "gke-cluster" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.cluster_name}-node-pool"
   location   = "${var.region}-b"
-  cluster    = google_container_cluster.primary.name
+  cluster    = google_container_cluster.gke-cluster.name
   node_count = var.node_count
 
   node_config {
